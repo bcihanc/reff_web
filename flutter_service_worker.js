daa69916bedfa28ed205b100ff9cf77178e8e972
cihan <b.cihancengiz@gmail.com>
2020-09-15 17:51:27 +0300

    Built web
    Branch: master
    Commit: 3fe436f6d2373a4010422f098db34be05d22766a (dirty)
    
    package:peanut 3.5.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "25c0c033e3e08140b23a055c34b365c7",
 "/": "25c0c033e3e08140b23a055c34b365c7",
-"main.dart.js": "20ee023492a431ae641ddd78c6745e44",
+"main.dart.js": "61d20e459e0a887c90d0ec123e9fec64",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
